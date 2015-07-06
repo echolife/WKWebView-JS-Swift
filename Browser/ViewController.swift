baeba2ef139cbbc93a5e67754100d6b993c6052f
王滔 <taowang@MacBook-Pro.local>
2015-07-06 13:59:40 +0800

    Initial Commit

---
+++ Browser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Browser
+//
+//  Created by 王滔 on 7/6/15.
+//  Copyright (c) 2015 王滔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
